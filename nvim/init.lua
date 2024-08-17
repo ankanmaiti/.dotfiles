e013583e695e5f4bf9bda32683aefc83ef602995
Ankan Maiti <ankanmaiti2@gmail.com>
2024-08-17 22:29:30 +0530

    feat: neovim submodule from my existing neovim repo

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,8 +1,1 @@
--- setup leader key
-vim.g.mapleader = " "
-vim.g.maplocalleader = " "
-
--- bootstrap lazy.nvim and plugins
-require("config.core")
-require("config.keymaps")
-require("config.lazy")
+require("neovim")
